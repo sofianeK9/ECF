34e20fd30635cd304d537e6130750d3fe83c8192
sofianek9 <kayasofiane7@gmail.com>
2023-09-13 14:36:07 +0200

    modifications

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Livre;
+use App\Entity\Auteur;
+use App\Entity\Genre;
 use DateTime;
 use Doctrine\ORM\Repository\RepositoryFactory;
 use Doctrine\Persistence\ManagerRegistry;
@@ -39,27 +42,75 @@ class TestController extends AbstractController
             'roles' => $roles,
             'userFalse' => $userFalse,
         ]);
-
-     
-
     }
 
     #[Route('/livre', name: 'app_test_livre')]
     public function livre(ManagerRegistry $doctrine): Response
     {
-
         $em = $doctrine->getManager();
         $livreRepository = $em->getRepository(Livre::class);
+        $auteurRepository = $em->getRepository(Auteur::class);
+        $genreRepository = $em->getRepository(Genre::class);
 
+        $livres = $livreRepository->findAllLivre();
 
+        $livre1 = $livreRepository->find(1);
 
+        $titreLorem = $livreRepository->findTitreLorem('lorem');
 
-        return $this->render('test/user.html.twig', [
-            'controller_name' => 'TestController',
-        
-        
-        
+        $listeLivre2 = $livreRepository->findBy([
+            'auteur' => 2,
+        ], [
+            'titre' => 'ASC',
         ]);
 
+
+        // Création d'un livre
+        $auteur2 = $auteurRepository->find(2);
+        $genre2 = $genreRepository->find(6);
+
+        $newBooks = new Livre();
+        $newBooks->setTitre('Totum autem id externum');
+        $newBooks->setAnneeEdition('2020');
+        $newBooks->setNombrePages('300');
+        $newBooks->setCodeIsbn('9790412882714');
+        $newBooks->setAuteur($auteur2);
+        $newBooks->addGenre($genre2);
+        $em->persist($newBooks);
+        $em->flush();
+         
+        // Mise à jour 
+
+        $genre5 = $genreRepository->find(5);
+        $Livre2 = $livreRepository->find(2);
+
+        $Livre2->setTitre('Aperiendum est igitur');
+        $Livre2->addGenre($genre5);
+        $em->persist($Livre2);
+        $em->flush();
+
+        // suppresion id 123
+
+        $id123 = $livreRepository->find(123);
+
+        if($id123){
+            $em->remove($id123);
+            $em->flush();
+        }
+
+
+
+        // Liste des livres dont la description est genre
+        $livreGenre = $livreRepository->findBooksByGenre('roman');
+
+        return $this->render('test/livre.html.twig', [
+            'controller_name' => 'TestController',
+            'livres' => $livres,
+            'livre1' => $livre1,
+            'titreLorem' => $titreLorem,
+            'listeLivre2' => $listeLivre2,
+            'livreGenre' => $livreGenre,
+
+        ]);
     }
 }
--- src/Repository/LivreRepository.php
+++ src/Repository/LivreRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Auteur;
 use App\Entity\Livre;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -21,28 +22,46 @@ class LivreRepository extends ServiceEntityRepository
         parent::__construct($registry, Livre::class);
     }
 
-//    /**
-//     * @return Livre[] Returns an array of Livre objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('l')
-//            ->andWhere('l.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('l.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findAllLivre(): array
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.titre IS NOT null')
+            ->orderBy('l.titre', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
 
-//    public function findOneBySomeField($value): ?Livre
-//    {
-//        return $this->createQueryBuilder('l')
-//            ->andWhere('l.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function findTitreLorem(string $keyword): array
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.titre LIKE :keyword')
+            ->setParameter('keyword', "%$keyword%")
+            ->orderBy('l.titre', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findBooksByGenre(string $genres): array
+    {
+        return $this->createQueryBuilder('l')
+            ->innerJoin('l.genres', 'genres')
+            ->andWhere('genres.nom LIKE :genres')
+            ->setParameter('genres', "%$genres%")
+            ->orderBy('l.titre', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+
+
+
+    //    public function findOneBySomeField($value): ?Livre
+    //    {
+    //        return $this->createQueryBuilder('l')
+    //            ->andWhere('l.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
